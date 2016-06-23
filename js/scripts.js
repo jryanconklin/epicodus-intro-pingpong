95e3553f41595eea97a9b87f76b4a1febc392b22
J. Ryan Conklin <jryanconklin@gmail.com>
2016-06-23 14:12:40 -0700

    Add First Specification: Take a Number Input and Count Up to that Number to Screen.

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,24 @@
 
 
 
+
+
+
+
+
+
 // User Interface Logic
 $(document).ready(function() {
   $("form#pingPong").submit(function(event) {
     event.preventDefault();
     var userNumber = $("input#userNumber").val();
+    var numbers = [];
 
-    alert(userNumber);
+    for ( var i = 0; i <= userNumber; i += 1 ) {
+      numbers.push(i);
+    }
+
+    $(".numbers").text(numbers);
+
   });
 });
