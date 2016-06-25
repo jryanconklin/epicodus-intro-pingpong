a56477564295ad76edb758f18647f1250dd94151
J. Ryan Conklin <jryanconklin@gmail.com>
2016-06-25 16:23:16 -0700

    Better Separate Business and User Interface Logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 // Business Logic
 
+var numbers = [];
+
 var ping = function(number) {
   if (number % 3 === 0 && number !== 0) {
     return true;
@@ -9,29 +11,33 @@ var ping = function(number) {
 var pong = function(number) {
   if (number % 5 === 0 && number !== 0) {
     return true;
-  }  
+  }
 };
+
+function pingPong(userNumber) {
+  for ( var i = 1; i <= userNumber; i += 1 ) {
+    if (ping(i) && pong(i)) {
+      numbers.push("pingpong");
+    } else if (ping(i)) {
+      numbers.push("ping");
+    } else if (pong(i)) {
+      numbers.push("pong");
+    } else
+      numbers.push(i);
+  }
+}
+
 
 // User Interface Logic
 $(document).ready(function() {
   $("form#pingPong").submit(function(event) {
     event.preventDefault();
     var userNumber = $("input#userNumber").val();
-    var numbers = [];
 
     if (userNumber <= 0) {
       $("#warning").html("<p>Please Enter a Number Above 1. Thank you!</p>");
-    }
-
-    for ( var i = 1; i <= userNumber; i += 1 ) {
-      if (ping(i) && pong(i)) {
-        numbers.push("pingpong");
-      } else if (ping(i)) {
-        numbers.push("ping");
-      } else if (pong(i)) {
-        numbers.push("pong");
-      } else
-        numbers.push(i);
+    } else {
+      pingPong(userNumber);
     }
 
     numbers.forEach(function(number) {
